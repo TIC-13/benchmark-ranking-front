98cdb5147db0400499bdb67e37895390201a40d3
ThalesBezerra21 <thalesoliveira.bezerra@gmail.com>
2024-04-26 14:04:04 -0300

    Separação de modelos por categoria no ranking simples

--- app/simpleRanking/hooks/useModels.ts
+++ app/simpleRanking/hooks/useModels.ts
@@ -1,10 +1,15 @@
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 
+export type Model = {
+    ml_model: string,
+    category: string
+}
+
 const useModels = () => 
     useQuery({
         queryKey: ["models"],
-        queryFn: async () => (await axios.get<string[]>("http://localhost:3030/inference/get/models")).data
+        queryFn: async () => (await axios.get<Model[]>("http://localhost:3030/inference/get/models")).data
     })
 
 export default useModels
--- app/simpleRanking/page.tsx
+++ app/simpleRanking/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React, { useRef, useState } from "react"
+import React, { useEffect, useRef, useState } from "react"
 
 import { DataTable } from "./data-table";
 import { columns } from "./columns";
@@ -8,7 +8,7 @@ import { TypographyH2 } from "@/components/typography/Typography";
 import useSimpleRanking from "./hooks/useSimpleRanking";
 import MainContainer from "@/components/custom/MainContainer";
 import { LoadingFullScreen } from "@/components/custom/LoadingFullScreen";
-import useModels from "./hooks/useModels";
+import useModels, { Model } from "./hooks/useModels";
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import {
@@ -29,8 +29,8 @@ import { Switch } from "@/components/ui/switch"
 import useQuantizations from "./hooks/useQuantizations";
 import { Label } from "@/components/ui/label";
 
-type Selectable = {
-    name: string,
+interface Selectable<T> {
+    value: T,
     isSelected: boolean
 }
 
@@ -44,20 +44,32 @@ export default function DataQueryLayer() {
 
     return (
         <PageLayer
-            modelsList={modelsQuery.data.map((x: string) => {
-                return { name: x, isSelected: true }
+            modelsList={modelsQuery.data.map(x => {
+                return { value: x, isSelected: true }
             })}
-            quantizationsList={quantizationsQuery.data.map((x: string) => {
-                return { name: x, isSelected: true }
+            quantizationsList={quantizationsQuery.data.map(x => {
+                return { value: x, isSelected: true }
             })}
         />
     )
 }
 
 type PageLayerProps = {
-    modelsList: Selectable[],
-    quantizationsList: Selectable[],
+    modelsList: Selectable<Model>[],
+    quantizationsList: Selectable<string>[],
 }
+
+type Category = {
+    value: string,
+    label: string
+}
+
+const CATEGORIES: Category[] = [
+    { value: "CLASSIFICATION", label: "Classificação" },
+    { value: "DETECTION", label: "Detecção" },
+    { value: "SEGMENTATION", label: "Segmentação" },
+    { value: "LANGUAGE", label: "Linguagem" }
+]
 
 function PageLayer({ modelsList, quantizationsList }: PageLayerProps) {
 
@@ -86,15 +98,36 @@ function PageLayer({ modelsList, quantizationsList }: PageLayerProps) {
                 </CardHeader>
                 <CardContent className="flex flex-col gap-y-5">
                     <Accordion type="multiple" className="max-w-l">
-                        <SelectionAccordionItem
-                            data={models}
-                            setData={setModels}
-                            title="Modelos"
-                        />
+                        <AccordionItem value="Modelos">
+                            <AccordionTrigger>Modelos</AccordionTrigger>
+                            <AccordionContent className="pl-5">
+                                {
+                                    CATEGORIES.map(category =>
+                                        <SelectionAccordionItem
+                                            data={models}
+                                            setData={setModels}
+                                            title={category.label}
+                                            getItemName={(item) => item.value.ml_model}
+                                            showItem={(item) => item.value.category === category.value}
+                                        />
+                                    )
+                                }
+                                <SelectionAccordionItem
+                                    data={models}
+                                    setData={setModels}
+                                    title="Outros"
+                                    getItemName={(item) => item.value.ml_model}
+                                    showItem={(item) => !CATEGORIES.map(x => x.value)
+                                        .includes(item.value.category)
+                                    }
+                                />
+                            </AccordionContent>
+                        </AccordionItem>
                         <SelectionAccordionItem
                             data={quantizations}
                             setData={setQuantizations}
                             title="Quantizações"
+                            getItemName={(item) => item.value}
                         />
                     </Accordion>
                     <div className="flex flex-row gap-x-5">
@@ -114,24 +147,23 @@ function PageLayer({ modelsList, quantizationsList }: PageLayerProps) {
                     </div>
                 </CardContent>
             </Card>
-
             <Ranking models={modelsToFetch} quantizations={quantizationsToFetch} />
         </MainContainer>
     )
 }
 
 type RankingLayerProps = {
-    models: Selectable[],
-    quantizations: Selectable[]
+    models: Selectable<Model>[],
+    quantizations: Selectable<string>[]
 }
 
 function Ranking({ models, quantizations }: RankingLayerProps) {
 
     const rankingQuery = useSimpleRanking(
         models.filter(x => x.isSelected)
-            .map(x => x.name),
+            .map(x => x.value.ml_model),
         quantizations.filter(x => x.isSelected)
-            .map(x => x.name),
+            .map(x => x.value),
     )
 
     if (rankingQuery.isLoading || rankingQuery.data === undefined)
@@ -143,26 +175,36 @@ function Ranking({ models, quantizations }: RankingLayerProps) {
 
 }
 
-type SelectionProps = {
-    data: Selectable[],
-    setData: React.Dispatch<React.SetStateAction<Selectable[]>>,
-    title: string
+interface SelectionProps<T> {
+    data: Selectable<T>[],
+    setData: React.Dispatch<React.SetStateAction<Selectable<T>[]>>,
+    title: string,
+    getItemName: (item: Selectable<T>) => string,
+    showItem?: (item: Selectable<T>) => boolean,
 }
 
-function SelectionAccordionItem({ data, setData, title }: SelectionProps) {
+function SelectionAccordionItem<T>({ data, setData, title, getItemName, showItem = (item) => true }: SelectionProps<T>) {
 
     const [checked, setChecked] = useState(true)
 
-    function onCheckedChange(newChecked: boolean){
+    function onCheckedChange(newChecked: boolean) {
         setData(data.map(x => {
-            return {...x, isSelected: newChecked}
+            return { ...x, isSelected: showItem(x)? newChecked: x.isSelected }
         }))
         setChecked(newChecked)
     }
 
+    useEffect(() => {
+        const showedItems = data.filter(x => showItem(x))
+        if(showedItems.every(x => x.isSelected))
+            setChecked(true)
+        if(showedItems.every(x => !x.isSelected))
+            setChecked(false)
+    }, [data])
+
     return (
-        <AccordionItem 
-            value={title} 
+        <AccordionItem
+            value={title}
         >
             <AccordionTrigger>
                 <div className="flex flex-1 flex-row justify-between items-center flex-wrap gap-y-5">
@@ -174,24 +216,25 @@ function SelectionAccordionItem({ data, setData, title }: SelectionProps) {
                             onCheckedChange={onCheckedChange}
                             onClick={(event) => event.stopPropagation()}
                         />
-                        <Label htmlFor="check">{checked? "Remover todos": "Selecionar todos"}</Label>
+                        <Label htmlFor="check">{checked ? "Remover todos" : "Selecionar todos"}</Label>
                     </div>
                 </div>
             </AccordionTrigger>
             <AccordionContent className="flex flex-row gap-x-3 gap-y-3 flex-wrap">
                 {
                     data.map((x, idx) =>
+                        showItem(x) &&
                         <Badge
                             variant={x.isSelected ? "default" : "outline"}
                             onClick={() =>
                                 setData((prev) => {
                                     const arr = [...prev]
-                                    arr[idx] = { name: x.name, isSelected: !prev[idx].isSelected }
+                                    arr[idx] = { ...arr[idx], isSelected: !prev[idx].isSelected }
                                     return arr
                                 })
                             }
                         >
-                            {x.name}
+                            {getItemName(x)}
                         </Badge>
                     )
                 }
@@ -200,15 +243,8 @@ function SelectionAccordionItem({ data, setData, title }: SelectionProps) {
     );
 }
 
-function arraysOfSelectableEquals(a: Selectable[], b: Selectable[]) {
-
-    if (a.length !== b.length) return false;
-
-    for (let i = 0; i < a.length; i++) {
-        if (a[i].name !== b[i].name || a[i].isSelected !== b[i].isSelected) return false;
-    }
-
-    return true;
+function arraysOfSelectableEquals(a: Selectable<any>[], b: Selectable<any>[]) {
+    return JSON.stringify(a) === JSON.stringify(b)
 }
 
 
