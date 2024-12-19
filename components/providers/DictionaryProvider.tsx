7ffd4c931113751f23b8b0989314c8aaaeee7b86
ThalesBezerra21 <thalesoliveira.bezerra@gmail.com>
2024-12-19 15:02:08 -0300

    Impedir uso de localStorage no servidor

--- components/providers/DictionaryProvider.tsx
+++ components/providers/DictionaryProvider.tsx
@@ -5,6 +5,19 @@ import { getDictionary, isLanguage, Language } from "@/app/dictionaries"
 
 const LANGUAGE_KEY = "language"
 
+function getStoredLanguage() {
+  if (typeof window !== "undefined") {
+    return localStorage.getItem(LANGUAGE_KEY)
+  }
+  return null
+}
+
+function storeLanguage(newLang: Language) {
+  if (typeof window !== "undefined") {
+    localStorage.setItem(LANGUAGE_KEY, newLang)
+  }
+}
+
 type Dictionary = Awaited<ReturnType<typeof getDictionary>>
 
 type DictionaryContextValue = {
@@ -21,13 +34,13 @@ export default function DictionaryProvider({
   children: React.ReactNode
 }) {
 
-  const savedLanguage = localStorage.getItem(LANGUAGE_KEY)
+  const savedLanguage = getStoredLanguage()
 
-  const [language, setLanguage] = useState<Language>(isLanguage(savedLanguage)? savedLanguage: "pt")
+  const [language, setLanguage] = useState<Language>(isLanguage(savedLanguage) ? savedLanguage : "pt")
   const dictionary = getDictionary(language)
 
   const updateLanguage = (newLang: Language) => {
-    localStorage.setItem(LANGUAGE_KEY, newLang)
+    storeLanguage(newLang)
     setLanguage(newLang)
   }
 
